5dea17a5a50996bc283bef0b88dce276a099c861
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2017-11-10 02:37:57 +0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM centos:latest
+RUN yum install -y epel-release && \
+        yum install -y git bzip2 fontconfig curl wget nodejs redis supervisor && \
+	cd /opt && \
+	git clone https://github.com/prerender/prerender.git && \
+	cd prerender && \
+	npm install && \
+	npm install prerender-redis-cache prerender-access-log --save && \
+	mv server.js server.js.bak && \
+	echo 'Prerender successfully installed'
+WORKDIR /opt/prerender
+
+VOLUME /var/log/prerender /var/log/redis /var/lib/redis
+
+EXPOSE 3000
+ENV REDIS_URL=redis://localhost:6379/0
+# 4 hours
+ENV PAGE_TTL=14400
+ENV PRERENDER_NUM_WORKERS=4
+ENV NUM_SOFT_ITERATIONS=10
+ENV PRERENDER_NUM_ITERATIONS=20
+
+ADD ./server.js /opt/prerender/server.js
+ADD ./supervisord.conf /etc/supervisord.conf
+RUN cat /etc/supervisord.conf
+CMD ["supervisord", "-c", "/etc/supervisord.conf", "--nodaemon"]
---
+++ server.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+var prerender = require('./lib');
+
+var server = prerender({
+    workers: process.env.PRERENDER_NUM_WORKERS,
+    iterations: process.env.PRERENDER_NUM_ITERATIONS,
+    accessLog: {
+          // Check out the file-stream-rotator docs for parameters
+          fileStreamRotator: {
+              filename: '/var/log/prerender/access-%DATE%.log',
+              frequency: 'daily',
+              date_format: 'YYYY-MM-DD',
+              verbose: false
+          },
+
+          // Check out the morgan docs for the available formats
+          morgan: {
+//              format: 'combined'
+		format: '[:date[clf]] ":method :url HTTP/:http-version" :status :response-time ms ":user-agent"'
+          }
+      }
+});
+
+
+server.use(prerender.sendPrerenderHeader());
+// server.use(prerender.basicAuth());
+// server.use(prerender.whitelist());
+server.use(prerender.blacklist());
+// server.use(prerender.logger());
+server.use(prerender.removeScriptTags());
+server.use(prerender.httpHeaders());
+// server.use(prerender.inMemoryHtmlCache());
+// server.use(prerender.s3HtmlCache());
+server.use(require('prerender-access-log'));
+server.use(require('prerender-redis-cache'));
+server.start();
